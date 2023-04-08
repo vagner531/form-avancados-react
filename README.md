146ee770e8a299f8d4322cce5d78d9f5d8def601
vagner santos <vagner131417@gmail.com>
2023-04-08 11:19:15 -0300

    feat: create validation and transformation

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Formulário com Validações Avançadas 
 
-Este é um projeto que utiliza as tecnologias React, Vite, Hook Form e Tailwind CSS para criar um formulário com validações avançadas. O formulário contém vários campos que serão validados e transformados de acordo com as regras definidas.
+Este é um projeto que utiliza as tecnologias React, Vite, Hook Form, Zod e Tailwind CSS para criar um formulário com validações avançadas. O formulário contém vários campos que serão validados e transformados de acordo com as regras definidas.
 
 Algumas das funcionalidades do formulário incluem:
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from '@hookform/resolvers/zod'
+
 import "./styles/global.css";
 
-import { useForm } from "react-hook-form";
+const createUserFormSchema = z.object({
+  name: z.string()
+  .nonempty('O nome é obrigatório')
+  .transform(name => {
+    return name.trim().split(' ').map(word => {
+      return word[0].toLocaleUpperCase().concat(word.substring(1))
+    }).join(' ')
+  }),
+  email: z.string()
+  .nonempty('O e-mail é obrigatório')
+  .email('Formato de e-mail inválido')
+  .toLowerCase(),
+  password: z.string()
+  .min(6, 'A senha precisa de no minimo 6 caracteres'),
+})
+
+type CreateUserFormData = z.infer<typeof createUserFormSchema>
 
 export function App() {
   const [output, setOutput] = useState("");
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm<CreateUserFormData>({
+    resolver: zodResolver(createUserFormSchema),
+  });
 
   function createUser(data: any) {
     setOutput(JSON.stringify(data, null, 2));
@@ -18,12 +40,23 @@ export function App() {
         className="flex flex-col gap-4 w-full max-w-xs"
       >
         <div className="flex flex-col gap-1">
+          <label htmlFor="">Nome</label>
+          <input
+            type="name"
+            className="border-zinc-800 shadow-sm rounded h-10 px-3 bg-zinc-900 text-white"
+            {...register("name")}
+          />
+          {errors.name && <span>{errors.name.message}</span>}
+        </div>
+
+        <div className="flex flex-col gap-1">
           <label htmlFor="">E-mail</label>
           <input
             type="email"
             className="border-zinc-800 shadow-sm rounded h-10 px-3 bg-zinc-900 text-white"
             {...register("email")}
           />
+          {errors.email && <span>{errors.email.message}</span>}
         </div>
 
         <div className="flex flex-col gap-1">
@@ -33,6 +66,7 @@ export function App() {
             className="border-zinc-800 shadow-sm rounded h-10 px-3 bg-zinc-900 text-white"
             {...register("password")}
           />
+          {errors.password && <span>{errors.password.message}</span>}
         </div>
 
         <button
