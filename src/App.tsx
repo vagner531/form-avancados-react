017fe65c0daa604d2eab6e887fe514b254f21981
vagner santos <vagner131417@gmail.com>
2023-04-07 22:45:09 -0300

    feat: create user function

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import "./styles/global.css";
 
+import { useForm } from 'react-hook-form'
+
+function createUser(data: any) {
+  console.log(data)
+}
+
 export function App() {
+    const { register, handleSubmit } = useForm();
+
   return (
-    <main className="h-screen bg-zinc-50 flex items-center justify-center">
-      <form action="" className="flex flex-col gap-4 w-full max-w-xs">
+    <main className="h-screen bg-zinc-950 text-zinc-300 flex items-center justify-center">
+      <form onSubmit={handleSubmit(createUser)} className="flex flex-col gap-4 w-full max-w-xs">
         <div className="flex flex-col gap-1">
           <label htmlFor="">E-mail</label>
           <input 
             type="email" 
-            name="email" 
             className="border-zinc-200 shadow-sm rounded h-10"
+            {...register('email')}
             />
         </div>
 
@@ -17,8 +25,8 @@ export function App() {
           <label htmlFor="">Senha</label>
           <input 
             type="password" 
-            name="password" 
             className="border-zinc-200 shadow-sm rounded h-10"
+            {...register('password')}
             />
         </div>
 
