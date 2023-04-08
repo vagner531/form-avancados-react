5b0cdf90060355c4ad0d5b99e69566de1c30e19a
vagner santos <vagner131417@gmail.com>
2023-04-08 12:44:33 -0300

    feat: add field arrays

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,60 @@
 import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from '@hookform/resolvers/zod'
-
 import "./styles/global.css";
 
-const createUserFormSchema = z.object({
-  name: z.string()
-  .nonempty('O nome é obrigatório')
-  .transform(name => {
-    return name.trim().split(' ').map(word => {
-      return word[0].toLocaleUpperCase().concat(word.substring(1))
-    }).join(' ')
-  }),
-  email: z.string()
-  .nonempty('O e-mail é obrigatório')
-  .email('Formato de e-mail inválido')
-  .toLowerCase(),
-  password: z.string()
-  .min(6, 'A senha precisa de no minimo 6 caracteres'),
-})
+import { useForm, useFieldArray } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-type CreateUserFormData = z.infer<typeof createUserFormSchema>
+const createUserFormSchema = z.object({
+  name: z
+    .string()
+    .nonempty("O nome é obrigatório")
+    .transform((name) => {
+      return name
+        .trim()
+        .split(" ")
+        .map((word) => {
+          return word[0].toLocaleUpperCase().concat(word.substring(1));
+        })
+        .join(" ");
+    }),
+  email: z
+    .string()
+    .nonempty("O e-mail é obrigatório")
+    .email("Formato de e-mail inválido")
+    .toLowerCase(),
+  password: z.string().min(6, "A senha precisa de no minimo 6 caracteres"),
+  techs: z
+    .array(
+      z.object({
+        title: z.string().nonempty("O titulo é obrigatório"),
+        knowledge: z.coerce.number().min(1).max(100),
+      })
+    )
+    .min(2, "Insira pelo menos 2 tecnologias"),
+});
+
+type CreateUserFormData = z.infer<typeof createUserFormSchema>;
 
 export function App() {
   const [output, setOutput] = useState("");
-  const { register, handleSubmit, formState: { errors } } = useForm<CreateUserFormData>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm<CreateUserFormData>({
     resolver: zodResolver(createUserFormSchema),
   });
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "techs",
+  });
+
+  function addNewTech() {
+    append({ title: "", knowledge: 0 });
+  }
 
   function createUser(data: any) {
     setOutput(JSON.stringify(data, null, 2));
@@ -46,7 +73,9 @@ export function App() {
             className="border-zinc-800 shadow-sm rounded h-10 px-3 bg-zinc-900 text-white"
             {...register("name")}
           />
-          {errors.name && <span>{errors.name.message}</span>}
+          {errors.name && (
+            <span className="text-red-500 text-sm">{errors.name.message}</span>
+          )}
         </div>
 
         <div className="flex flex-col gap-1">
@@ -56,7 +85,9 @@ export function App() {
             className="border-zinc-800 shadow-sm rounded h-10 px-3 bg-zinc-900 text-white"
             {...register("email")}
           />
-          {errors.email && <span>{errors.email.message}</span>}
+          {errors.email && (
+            <span className="text-red-500 text-sm">{errors.email.message}</span>
+          )}
         </div>
 
         <div className="flex flex-col gap-1">
@@ -66,9 +97,58 @@ export function App() {
             className="border-zinc-800 shadow-sm rounded h-10 px-3 bg-zinc-900 text-white"
             {...register("password")}
           />
-          {errors.password && <span>{errors.password.message}</span>}
+          {errors.password && (
+            <span className="text-red-500 text-sm">
+              {errors.password.message}
+            </span>
+          )}
         </div>
 
+        <div className="flex flex-col gap-1">
+          <label htmlFor="" className="flex items-center justify-between">
+            Tecnologias
+            <button
+              type="button"
+              onClick={addNewTech}
+              className="text-emerald-500 text-sm"
+            >
+              Adicionar
+            </button>
+          </label>
+          {fields.map((field, index) => {
+            return (
+              <div className="flex gap-2" key={field.id}>
+                <div className="flex-1 flex flex-col gap-1">
+                  <input
+                    type="text"
+                    className=" border-zinc-800 shadow-sm rounded h-10 px-3 bg-zinc-900 text-white"
+                    {...register(`techs.${index}.title`)}
+                  />
+                  {errors.techs?.[index]?.title && (
+                    <span className="text-red-500 text-sm">
+                      {errors.techs?.[index]?.title?.message}
+                    </span>
+                  )}
+                </div>
+                <div className="flex flex-col gap-1">
+                  <input
+                    type="number"
+                    className="w-16 flex-1 border-zinc-800 shadow-sm rounded h-10 px-3 bg-zinc-900 text-white"
+                    {...register(`techs.${index}.knowledge`)}
+                  />
+                  {errors.techs?.[index]?.knowledge && (
+                    <span className="text-red-500 text-sm">
+                      {errors.techs?.[index]?.knowledge?.message}
+                    </span>
+                  )}
+                </div>
+              </div>
+            );
+          })}
+          {errors.techs && (
+            <span className="text-red-500 text-sm">{errors.techs.message}</span>
+          )}
+        </div>
         <button
           type="submit"
           className="bg-emerald-500 rounded font-semibold text-white h-10 hover:bg-emerald-600"
